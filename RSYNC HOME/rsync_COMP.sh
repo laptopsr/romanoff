a6145a5436ade4298d9a1bd5c2785ac9d736f08d
Roman Sizov <laptopsr@gmail.com>
2022-02-24 15:03:52 +0200

    rsync

--- RSYNC HOME/rsync_COMP.sh
+++ RSYNC HOME/rsync_COMP.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
+
+# tail -f /tmp/rsync.log
+
 # 192.168.1.110 - Raspberry PI SERVER
 # 192.168.1.190 - HP Omni
 # 192.168.1.191 - Acer
 # 192.168.1.192 - Compaq
+# 192.168.1.193 - ASUS
 
+
+TMP_FILE="/tmp/rsync_main_ip.txt"
 IP=$(hostname -I | awk '{print $1}')
 
 # THIS COMP
 COMP_DIR=$HOME
+
+if [ $IP == "192.168.1.193" ]; then
+	COMP_DIR="/media/laptopsr/sdb1"
+fi
 
 # SERVER
 SERVER_IP="192.168.1.110"
@@ -18,7 +28,7 @@ SERVER_DIR="/media/pi/07b6b844-0c3a-4e62-bb3d-b51cc61a5c0a"
 SYNC_DIR="MOE"
 
 echo "----PULL FROM SERVER: rsync -av --delete $SERVER_USER@$SERVER_IP:$SERVER_DIR/$SYNC_DIR $COMP_DIR" > /tmp/rsync.log
-rsync -av --delete $SERVER_USER@$SERVER_IP:$SERVER_DIR/$SYNC_DIR $COMP_DIR --log-file=/tmp/rsync.log
+rsync -avu --delete $SERVER_USER@$SERVER_IP:$SERVER_DIR/$SYNC_DIR $COMP_DIR --log-file=/tmp/rsync.log
 
 #modify,create,delete,move,open,moved_to,moved_from
 inotifywait -e create,modify,delete,move -r -m $COMP_DIR/$SYNC_DIR --exclude '\.sh$' |
@@ -48,8 +58,17 @@ do
 	echo "-------------"
 	
 	#continue
-	echo "----PUSH TO SERVER: rsync -av --delete $COMP_DIR/$SYNC_DIR $SERVER_USER@$SERVER_IP:$SERVER_DIR" > /tmp/rsync.log
-	rsync -av --delete $COMP_DIR/$SYNC_DIR $SERVER_USER@$SERVER_IP:$SERVER_DIR --log-file=/tmp/rsync.log
+
+	if ssh $SERVER_USER@$SERVER_IP "test -e $TMP_FILE"; then
+		echo "-----$TMP_FILE IS EXISTS-----" > /tmp/rsync.log
+	else
+		echo "---SEND FILE TO SERVER: $TMP_FILE" > /tmp/rsync.log
+		echo $IP > $TMP_FILE | scp $TMP_FILE $SERVER_USER@$SERVER_IP:/tmp
+		rm $TMP_FILE
+	fi
+
+	echo -en "\n----------------\nPUSH TO SERVER\nrsync -av --delete $COMP_DIR/$SYNC_DIR $SERVER_USER@$SERVER_IP:$SERVER_DIR\n----------------\n\n" > /tmp/rsync.log
+	rsync -avu --delete $COMP_DIR/$SYNC_DIR $SERVER_USER@$SERVER_IP:$SERVER_DIR --log-file=/tmp/rsync.log
 
 done
 
--- RSYNC HOME/rsync_SERVER.sh
+++ RSYNC HOME/rsync_SERVER.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # THIS FILE FOR SERVER
 
+TMP_FILE="/tmp/rsync_main_ip.txt"
 IP=$(hostname -I | awk '{print $1}')
 MySyncDirectory="/media/pi/07b6b844-0c3a-4e62-bb3d-b51cc61a5c0a/MOE"
 
@@ -13,11 +14,13 @@ MySyncDirectory="/media/pi/07b6b844-0c3a-4e62-bb3d-b51cc61a5c0a/MOE"
 SUB_1=("192.168.1.190" "laptopsr" "~") # HP Omni
 SUB_2=("192.168.1.191" "laptopsr" "~") # Acer
 SUB_3=("192.168.1.192" "laptopsr" "~") # Compaq
+SUB_4=("192.168.1.193" "laptopsr" "/media/laptopsr/sdb1") # ASUS
 
 MAIN_ARRAY=(
   SUB_1[@]
   SUB_2[@]
   SUB_3[@]
+  SUB_4[@]
 )
 
 #modify,create,delete,move,open,moved_to,moved_from
@@ -27,10 +30,6 @@ do
 
 	if [[ $file == ".goutputstream"* ]] && [ $action != "MODIFY" ]; then
 	  continue
-	fi
-
-	if [ $action == "CREATE" ] || [ $action == "CREATE,ISDIR" ]; then
-		continue
 	fi
 
 	if [[ (($action == "MOVED_FROM,ISDIR") && ($file == "untitled folder")) ]]; then
@@ -49,6 +48,8 @@ do
 	
 	#continue
 	
+	MAIN_IP_FROM=$(cat $TMP_FILE)
+
 	# Loop and print it.  Using offset and length to extract values
 	COUNT=${#MAIN_ARRAY[@]}
 	for ((i=0; i<$COUNT; i++))
@@ -57,19 +58,23 @@ do
 		SUB_USER=${!MAIN_ARRAY[i]:1:1}
 		SUB_DEST=${!MAIN_ARRAY[i]:2:1}
 
+		if [ "$SUB_IP" == "$MAIN_IP_FROM" ]; then
+			echo -en "\n----------------\n----NOT SEND TO BACK: ($MAIN_IP_FROM)\n----------------\n\n" > /tmp/rsync.log
+			rm $TMP_FILE
+			continue
+		fi
+
 		if ping -c 1 $SUB_IP > /dev/null 2>&1;
 		then
 			if [ "$IP" != "$SUB_IP" ]; then
-				echo "----ACTION: ($action)" > /tmp/rsync.log
-				echo "------FILE: ($file)" > /tmp/rsync.log
-				echo "---SEND TO: $SUB_USER@$SUB_IP:$SUB_DEST" > /tmp/rsync.log
-				rsync -av --delete $MySyncDirectory $SUB_USER@$SUB_IP:$SUB_DEST --log-file=/tmp/rsync.log
+				echo -en "\n----------------\n----ACTION: ($action)\n----FILE: ($file)\n----SEND TO: $SUB_USER@$SUB_IP:$SUB_DEST\n----------------\n\n" > /tmp/rsync.log
+				rsync -avu --delete $MySyncDirectory $SUB_USER@$SUB_IP:$SUB_DEST --log-file=/tmp/rsync.log
 			fi
 			
 		else
-			echo "ERROR connection $SUB_IP"
+			echo "ERROR connection $SUB_IP" > /tmp/rsync.log
 		fi
-	done
+	done	
 done
 
 
